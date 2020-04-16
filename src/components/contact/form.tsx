52bcb4dbf85839b7be26cbf5afd962feae8da9fc
Craig Wheeler <craig.wheeler@gooee.com>
2020-04-16 10:08:05 -0400

    basic email form

--- src/components/contact/form.tsx
+++ src/components/contact/form.tsx
@@ -1,7 +1,29 @@
 import * as React from 'react';
 
 const ContactForm = () => {
-  return <div>Contact Form</div>;
+  return (
+    <form name="contact" action="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+      <div data-netlify-recaptcha="true"></div>
+    </form>
+  );
 };
 
 export default ContactForm;
