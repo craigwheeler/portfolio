fdda58049eae7d548f474f8fe1824f8408304b9e
Craig Wheeler <craig.wheeler@gooee.com>
2020-04-16 16:17:03 -0400

    update test folders

--- src/components/contact/__tests__/contact.spec.tsx
+++ src/components/contact/__tests__/contact.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import ContactPage from './';
+import ContactPage from '../../contact';
 
 describe('Contact', () => {
   it('should render contact page', () => {
--- src/components/home/__tests__/home.spec.tsx
+++ src/components/home/__tests__/home.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import HomePage from './';
+import HomePage from '../../home';
 
 describe('Home', () => {
   it('should render home page', () => {
