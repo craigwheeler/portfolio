3483ba1dd0c44b10ae35ddf1baa3a28f38aa177f
Craig Wheeler <craig.wheeler@gooee.com>
2020-04-16 09:11:00 -0400

    fix test

--- src/components/contact/contact.spec.tsx
+++ src/components/contact/contact.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import ContactPage from '../contact';
+import ContactPage from './';
 
 describe('Contact', () => {
   it('should render contact page', () => {
